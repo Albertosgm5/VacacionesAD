b0f5c2513e3f37963484a33cdcf0bdca741da0ff
Dani <Dani@DESKTOP-5NHDQ12>
2020-05-26 20:51:21 +0200

    Base de datos en proceso

--- src/app/trabajador-service.service.ts
+++ src/app/trabajador-service.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
 export class TrabajadorServiceService {
-
-  constructor() { }
+  
 }
