6b2076aae7a389f99e0f3ec93e9e00aafba008d8
Dani <Dani@DESKTOP-5NHDQ12>
2020-05-28 20:23:50 +0200

    Editar funcionando completamente

--- src/app/editar-trabajador/editar-trabajador.component.ts
+++ src/app/editar-trabajador/editar-trabajador.component.ts
@@ -9,6 +9,7 @@ import { TrabajadorServiceService } from '../trabajador-service.service'
 })
 export class EditarTrabajadorComponent implements OnInit {
 
+  encontrado: boolean = false;
   fecha: Date;
   trabajador: Trabajador;
   trabajador2: Trabajador;
@@ -32,13 +33,14 @@ export class EditarTrabajadorComponent implements OnInit {
         this.trabajador.fecha = this.fecha;
         console.log(this.trabajador);
         this.json.updateTrabajador(this.trabajador, "http://localhost:3000/trabajadores").subscribe(trabajador => this.trabajador2);
-
+        this.encontrado = true;
       }
     }
-    if (this.trabajador2 != null) {
-      this.mensaje = "Trabajador editado";
+    if (this.encontrado) {
+      this.mensaje = "Se ha editado el trabajador";
+      this.encontrado = false;
     } else {
-      this.mensaje = "No se encontro al trabajador";
+      this.mensaje = "No se encontro ningun trabajador con ese DNI";
     }
   }
     ngOnInit(): void {
