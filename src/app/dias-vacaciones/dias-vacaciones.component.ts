9020aebe55bedc0c35891ae12c13a41ef4637843
Dani <Dani@DESKTOP-5NHDQ12>
2020-05-28 15:02:55 +0200

    Vacaciones Funcionando. Ya funcionan todos los metodos

--- src/app/dias-vacaciones/dias-vacaciones.component.ts
+++ src/app/dias-vacaciones/dias-vacaciones.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Trabajador } from '../Trabajador';
 import { Vacaciones } from '../Vacaciones';
-
+import { TrabajadorServiceService } from '../trabajador-service.service'
 @Component({
   selector: 'app-dias-vacaciones',
   templateUrl: './dias-vacaciones.component.html',
@@ -9,26 +9,34 @@ import { Vacaciones } from '../Vacaciones';
 })
 export class DiasVacacionesComponent implements OnInit {
 
-   fecha: Date;
  trabajador: Trabajador;
  vacaciones: Vacaciones;
  solicitud : number;
- dni:string;
-  trabajadores = [new Trabajador('Juan', '14535346gc', new Date ('2020/05/26'), 0,1), new Trabajador('Albert', '149939929gc', new Date ('2020/05/26'), 0,1)];
+  dni: string;
+  trabajadores = [];
+
+  constructor(public json: TrabajadorServiceService) {
+    this.json.getJson('http://localhost:3000/trabajadores').subscribe((res: any) => {
+      this.trabajadores = res;
+      console.log(this.trabajadores);
+    })
+  }
 
 
    guardar() {
     for(let x=0;x<this.trabajadores.length;x++){
-        if (this.trabajadores[x].getDni() === this.dni){
-          if(this.solicitud <= this.trabajador.getDiasAcumulados()){
-            alert('Vacaciones concedidas');
-            this.trabajador.setDiasAcumulados(this.solicitud - this.trabajador.getDiasAcumulados());
+      if (this.trabajadores[x].dni === this.dni) {
+        this.trabajador = this.trabajadores[x];
+        if (this.solicitud <= this.trabajador.diasAcumulados && this.solicitud != 0) {
+          this.trabajador.diasAcumulados = (this.trabajador.diasAcumulados -this.solicitud);
+          this.json.updateTrabajador(this.trabajador, "http://localhost:3000/trabajadores").subscribe(trabajador => this.trabajador);
+          alert('Vacaciones concedidas');
           }else{
             alert('Vacaciones denegadas');
           }
         }
     }
-    if (this.trabajador.getDni() == "") {
+    if (this.trabajador.dni == "") {
     alert('No existe el dni de trabajador ingresado')
     }
   }
